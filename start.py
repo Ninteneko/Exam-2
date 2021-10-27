519bce4168cd09b3a9361a666e80f6892a72c2aa
lukejones88 <lukejones2088@gmail.com>
2021-10-27 15:57:31 -0700

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,22 @@
 # file for Exam 2
+forbidden = [
+    [0,0,0,0],
+    [1,1,1,1],
+    [2,2,2,2]
+]
+
+goal = [1,2,1,2]
+code = [1,2,1,0]
+
 numberMisplaced(code) #heuristic
-goalcheck(code)
 changeDigit(code, index)
+
+def goalcheck(code):
+    for i in range(len(code)):
+        if code[i] != goal[i]:
+            return False
+    return True
+
 
 def sortFringe(fringe):
 	fringe.sort(key=lambda item: (item["depth"] + numberMisplaced(item['code'])))
@@ -26,18 +41,31 @@ def search(startNode):
 
 	return endNode
 
-addSuccessor(currentNode, fringe)
+addSuccessor(currentNode, fringe) #Note how a node is defined in start()
+
+def addSuccessor(currentNode, fringe):
+	forbiddenCodes = [
+		[0, 0, 0, 0],
+		[1, 1, 1, 1],
+		[2, 2, 2, 2]
+	]
+	for index in range(4):
+		newCode = changeDigit(currentNode["code"], index)
+		if not newCode in forbiddenCodes:
+			newNode = {
+				"code": newCode ,
+				"depth": currentNode["depth"] + 1,
+				"plan": currentNode["plan"] + [newCode],
+				"parent": currentNode
+			}
+			fringe.append(newNode)
 
 def start(input):
 	startNode = {
 		"code": input,
 		"depth": 0,
-<<<<<<< Updated upstream
-		"plan": []
-=======
 		"plan": [input], #a list of past codes
 		"parent": None
->>>>>>> Stashed changes
 	}
 
 	endNode = search(startNode)
