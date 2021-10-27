6679b67f4612359925069cd44955fc3c32f0dd24
Diegonz02 <diego.gonzales963@myci.csuci.edu>
2021-10-27 11:39:15 -0700

    ChangeDigit updated
    ChangeDigit updated

--- start.py
+++ start.py
@@ -1,2 +1,9 @@
 # file for Exam 2
 # Diego Gonzales
+changeDigit(code)
+number = list(code)
+for i in range(4):
+    for j in range(1, 4):
+        number[i] = ((number[i]+1)%3)
+        if j > 1:
+            continue
