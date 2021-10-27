46079846b28d51e2b87afb9336519178839db89b
unknown <yarelit.mendoza@gmail.com>
2021-10-27 11:48:23 -0700

    pulled from malakai branch and and merged with mine

--- start.py
+++ start.py
@@ -8,15 +8,53 @@ forbidden = [
 goal = [1,2,1,2]
 code = [1,2,1,0]
 
-#numberMisplaced(code) #heuristic
+numberMisplaced(code) #heuristic
+changeDigit(code, index)
+
 def goalcheck(code):
     for i in range(len(code)):
         if code[i] != goal[i]:
             return False
     return True
 
-#changeDigit(code, index)
-#sortFringe(fringe)
-#search(startNode)
-#addSuccessor(currentNode, fringe)
-#start()
+
+def sortFringe(fringe):
+	fringe.sort(key=lambda item: (item["depth"] + numberMisplaced(item['code'])))
+
+def search(startNode):
+	endNode = None
+	fringe = [startNode]
+	visited = []
+
+	while fringe and not endNode:
+		sortFringe(fringe)
+		currentNode = fringe.pop(0)
+
+		if currentNode['code'] in visited:
+			continue
+		visited.append(currentNode['code'])
+
+		if goalcheck(currentNode['code']):
+			endNode = currentNode
+		else:
+			addSuccessor(currentNode, fringe)
+
+	return endNode
+
+addSuccessor(currentNode, fringe)
+
+def start():
+	startNode = {
+		"code": [0, 1, 2, 0],
+		"depth": 0,
+		"plan": []
+	}
+
+	goalCode = [1, 2, 1, 2]
+
+	endNode = search(startNode, goalCode)
+
+	if endNode:
+		print("Plan: ", endNode['plan'])
+	else:
+		print("No Solution Found")
